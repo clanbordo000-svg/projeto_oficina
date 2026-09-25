e5cd3dce53bc4cfc6a679da2cb079e4de9255c5a
Joao Carvalho <clanbordo000@gmail.com>
2026-09-25 20:41:04 -0300

    Create moto.js

---
+++ moto.js
@@ -0,0 +1,11 @@
+class Moto {
+ constructor(modelo, ano) {
+ this.modelo = modelo;
+ this.ano = ano;
+ }
+}
+
+
+const moto = new Moto("Twister 300f", 2023);
+console.log(moto.modelo);
+console.log(moto.ano);
